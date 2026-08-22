b80f5d698ca64086a2675010e7a918567b7a420f
Basu Saxena <basusaxena2003@gmail.com>
2026-08-22 16:51:00 +0530

    add config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,23 +4,32 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/Siddharth9101/olx-api/internal/config"
 )
 
 func main() {
+	// loading config
+	cfg := config.MustLoad()
+	// initializing new router/mux
 	mux := http.NewServeMux();
+
+	// defining health endpoint
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"status": "ok"}`))
 	})
 
+	// initializing new server
 	srv := http.Server{
-		Addr: ":8080",
+		Addr: ":"+ cfg.Port,
 		Handler: mux,
 		ReadTimeout: time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout: time.Second * 60,
 	}
+	log.Println("server is listning on "+srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server failed: %v", err)
 	}
